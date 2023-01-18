e2ff759ca96997358e3e7c695370630978654642
batuhanbolat <92889711+batuhanbolat@users.noreply.github.com>
2023-01-18 19:53:31 +0300

    Update conversational_chatbot.py

--- conversational_chatbot.py
+++ conversational_chatbot.py
@@ -174,7 +174,7 @@ def str_to_tokens( sentence : str ):
 
 enc_model , dec_model = make_inference_models()
 for epoch in range( encoder_input_data.shape[0] ):
-    states_values = enc_model.predict( str_to_tokens( st.text_input('User: ','Input:',key='placeholder') ) )
+    states_values = enc_model.predict( str_to_tokens( st.text_input("User: ") ) )
     empty_target_seq = np.zeros( ( 1 , 1 ) )
     empty_target_seq[0, 0] = output_word_dict['start']
     stop_condition = False
